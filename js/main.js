66020240eb07c1833dca78ad97cd7b300e0d3951
梶西 宏海 <kajinishi.0729@gmail.com>
2018-02-17 11:48:27 +0900

    文字追加

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ function setup(){
   var taximg=loadImage("../image/税金.png");
   image(mineimg,10,10);
   image(taximg,110,110);
+  text("公共財ゲーム",windowWidth/2,windowHeight/2);
 }
 function touchMoved(){
   line(touchX,touchY,ptouchX,ptouchY);
