6635f6d77e66f88dc5e010230c8dd9d070fb06f3
梶西 宏海 <kajinishi.0729@gmail.com>
2018-02-17 11:29:22 +0900

    画像処理お試し

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  stroke(0);
+  background(200);
+  var img=loadImage("img/私財.png");
+  image(img,10,10);
+}
+function touchMoved(){
+  line(touchX,touchY,ptouchX,ptouchY);
+  return false;
+}
+function mouseDragged(){
+  line(mouseX,mouseY,pmouseX,pmouseY);
+}
