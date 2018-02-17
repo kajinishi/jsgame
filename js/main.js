c89b494ebc129fd2a44f547d7a43c5a712c68e02
梶西 宏海 <kajinishi.0729@gmail.com>
2018-02-17 11:38:57 +0900

    パス修正

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function setup(){
   createCanvas(windowWidth,windowHeight);
   stroke(0);
   background(200);
-  var img=loadImage("img/私財.png");
+  var img=loadImage("../img/私財.png");
   image(img,10,10);
 }
 function touchMoved(){
