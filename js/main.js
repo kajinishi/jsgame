8d0cc8fb8d0f0edcaf14cd1bca750d857a798807
梶西 宏海 <kajinishi.0729@gmail.com>
2018-02-17 11:52:06 +0900

    文字追加

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ function setup(){
   createCanvas(windowWidth,windowHeight);
   stroke(0);
   background(200);
-  var mineimg=loadImage("../image/私財.png");
-  var taximg=loadImage("../image/税金.png");
+  var mineimg=loadImage("./image/私財.png");
+  var taximg=loadImage("./image/税金.png");
   image(mineimg,10,10);
   image(taximg,110,110);
   text("公共財ゲーム",windowWidth/2,windowHeight/2);
